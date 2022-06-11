aaecc9ddfc770f09342b92d9b867ddf8f79d440a
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-06-11 12:58:42 +0600

    #5 Создание компонента Items

---
+++ src/components/Items/Items.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Item from "../Item/Item";
+
+const Items = (props) => {
+  return props.items.map((item, index) => {
+    return <Item
+      key={item.id}
+      image={props.ITEMS[index].image}
+      price={props.ITEMS[index].price}
+      name={item.name}
+    />
+  });
+};
+
+export default Items;
