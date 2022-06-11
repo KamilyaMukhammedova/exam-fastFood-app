b5e8a541f3baba75da60bc96227e1067efd9977d
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-06-11 12:40:49 +0600

    #2 Создание массива ITEMS и добавление картинок в assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import hamburgerImage from './assets/hamburger.png';
+import cheeseburgerImage from './assets/cheeseburger.png';
+import friesImage from './assets/fries.png';
+import coffeeImage from './assets/coffee.png';
+import teaImage from './assets/tea.png';
+import colaImage from './assets/cola.png';
 
-function App() {
+const ITEMS = [
+  {name: 'Hamburger', price: 80, image: hamburgerImage},
+  {name: 'Cheeseburger', price: 90, image: cheeseburgerImage},
+  {name: 'Fries', price: 45, image: friesImage},
+  {name: 'Coffee', price: 70, image: coffeeImage},
+  {name: 'Tea', price: 50, image: teaImage},
+  {name: 'Cola', price: 40, image: colaImage},
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
-}
+};
 
 export default App;
